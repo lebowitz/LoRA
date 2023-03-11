3614c5bb6cd6a677085d004161c8499254c61cfd
lebowitz@alum.mit.edu <lebowitz@alum.mit.edu>
2023-03-10 22:10:44 -0500

    copy pasta pasta?

--- runpod_sd_scripts.sh
+++ runpod_sd_scripts.sh
@@ -1,35 +1,26 @@
 pushd /workspace
 
-pip3 install --upgrade diffusers[torch] --quiet
-pip3 install discord-webhook --quiet
-pip3 install tensorflow --quiet
-pip3 install accelerate==0.16 --quiet
-pip3 install triton --quiet
+pip3 install --upgrade diffusers[torch] --quiet >> /workspace/log.txt
+pip3 install discord-webhook --quiet >> /workspace/log.txt
+pip3 install tensorflow --quiet >> /workspace/log.txt
+pip3 install accelerate==0.16 --quiet >> /workspace/log.txt
+pip3 install triton --quiet  >> /workspace/log.txt
 
 apt install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev zlib1g-dev lzma  >> /workspace/log.txt
-liblzma-dev python3-venv 
-wget https://www.python.org/ftp/python/3.10.9/Python-3.10.9.tgz 
+liblzma-dev python3-venv  >> /workspace/log.txt
+wget https://www.python.org/ftp/python/3.10.9/Python-3.10.9.tgz  >> /workspace/log.txt
 tar -xf Python-3.10.*.tgz  >> /workspace/log.txt
-cd Python-3.10.*/ 
- 
-./configure --enable-optimizations  >> /workspace/log.txt
+
+pushd Python-3.10.9/ 
+./configure --enable-optimizations >> /workspace/log.txt
 make –j 16  >> /workspace/log.txt
 make altinstall  >> /workspace/log.txt
 popd 
-pushd /workspace
-apt install --quiet -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev zlib1g-dev lzma   >> /workspace/log.txt
-liblzma-dev python3-venv  >> /workspace/log.txt
-wget https://www.python.org/ftp/python/3.10.9/Python-3.10.9.tgz -O Python-3.10.9.tgz  >> /workspace/log.txt
-tar -xf Python-3.10.9.tgz --quiet   >> /workspace/log.txt
-cd Python-3.10.9/
-./configure --enable-optimizations  >> /workspace/log.txt
-make –j 16 # cpu core
-make altinstall  >> /workspace/log.txt
-popd
+
 git clone https://github.com/kohya-ss/sd-scripts
 pushd /workspace/sd-scripts
 rm -rf venv
-python3.10 -m venv venv   >> /workspace/log.txt
+python3.10 -m venv venv  >> /workspace/log.txt
 source venv/bin/activate
 pip install --upgrade -r requirements.txt   >> /workspace/log.txt 
 pip install xformers  >> /workspace/log.txt 
