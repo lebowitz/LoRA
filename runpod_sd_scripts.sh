16471ceda7439c968e6a4d6f47bdce0a3cb88f8a
Craig Lebowitz <craiglebowitz@gmail.com>
2023-03-12 16:31:55 -0400

    Update runpod_sd_scripts.sh

--- runpod_sd_scripts.sh
+++ runpod_sd_scripts.sh
@@ -1,46 +1,54 @@
 pushd /workspace
 
-pip3 install --upgrade diffusers[torch] --quiet >> /workspace/log.txt
-pip3 install discord-webhook --quiet >> /workspace/log.txt
-pip3 install tensorflow --quiet >> /workspace/log.txt
-pip3 install accelerate==0.16 --quiet >> /workspace/log.txt
-pip3 install triton --quiet  >> /workspace/log.txt
+pip3 install --upgrade diffusers[torch] --quiet >> /workspace/sd_scripts_log.txt
+pip3 install discord-webhook --quiet >> /workspace/sd_scripts_log.txt
+pip3 install tensorflow --quiet >> /workspace/sd_scripts_log.txt
+pip3 install accelerate==0.16 --quiet >> /workspace/sd_scripts_log.txt
+pip3 install triton --quiet >> /workspace/sd_scripts_log.txt
 
-apt install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev zlib1g-dev lzma  >> /workspace/log.txt
-liblzma-dev python3-venv  >> /workspace/log.txt
-wget https://www.python.org/ftp/python/3.10.9/Python-3.10.9.tgz  >> /workspace/log.txt
-tar -xf Python-3.10.*.tgz  >> /workspace/log.txt
+if [ ! -f /workspace/Python-3.10.9.tgz ]; then     
 
-pushd Python-3.10.9
+  apt install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev zlib1g-dev lzma  >> /workspace/log.txt
+  liblzma-dev python3-venv >> /workspace/sd_scripts_log.txt
+  wget https://www.python.org/ftp/python/3.10.9/Python-3.10.9.tgz >> /workspace/sd_scripts_log.txt
+  tar -xf Python-3.10.9.tgz >> /workspace/sd_scripts_log.txt
 
-./configure --enable-optimizations >> /workspace/log.txt
-make –j 16  >> /workspace/log.txt
-make altinstall  >> /workspace/log.txt
+  pushd Python-3.10.9
 
-popd 
+  ./configure --enable-optimizations >> /workspace/sd_scripts_log.txt
+  make –j 16 >> /workspace/sd_scripts_log.txt
+  make altinstall >> /workspace/sd_scripts_log.txt
 
-rm -rf sd-scripts
-git clone https://github.com/kohya-ss/sd-scripts
-pushd /workspace/sd-scripts
+  popd 
 
-rm -rf venv
-python3.10 -m venv venv  >> /workspace/log.txt
-source venv/bin/activate
+  rm -rf sd-scripts
+  git clone https://github.com/kohya-ss/sd-scripts >> /workspace/sd_scripts_log.txt
+  pushd /workspace/sd-scripts
 
-pip install --upgrade -r requirements.txt   >> /workspace/log.txt 
-pip install xformers  >> /workspace/log.txt 
-pip install triton  >> /workspace/log.txt
+  rm -rf venv
+  python3.10 -m venv venv >> /workspace/sd_scripts_log.txt
+  source venv/bin/activate >> /workspace/sd_scripts_log.txt
 
-popd
+  pip install --upgrade -r requirements.txt >> /workspace/sd_scripts_log.txt 
+  pip install xformers >> /workspace/sd_scripts_log.txt
+  pip install triton >> /workspace/sd_scripts_log.txt
 
-# the sd_script fail with _lzma error if you don't do this part
-apt-get --yes --quiet install liblzma-dev lzma >> /workspace/log.txt
+  popd
 
-# recompile/install python??!!
-# no idea, but this hack is necessary to work around the missing _lzma module error popd
+  # the sd_script fail with _lzma error if you don't do this part
+  apt-get --yes --quiet install liblzma-dev lzma >> /workspace/sd_scripts_log.txt
 
-pushd /workspace/Python-3.10.9/
-./configure --enable-optimizations  >> /workspace/log.txt
-make –j 16  >> /workspace/log.txt
-make altinstall  >> /workspace/log.txt
-popd
+  # recompile/install python??!!
+  # no idea, but this hack is necessary to work around the missing _lzma module error popd
+
+  pushd /workspace/Python-3.10.9/
+  ./configure --enable-optimizations >> /workspace/sd_scripts_log.txt
+  make –j 16 >> /workspace/sd_scripts_log.txt
+  make altinstall >> /workspace/sd_scripts_log.txt
+  popd
+
+else
+
+  echo "Python 3.10.9 already installed."
+
+fi
