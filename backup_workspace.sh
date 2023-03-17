a413c49e4f735c6f216692b9f20767b79ec6904c
Craig Lebowitz <craiglebowitz@gmail.com>
2023-03-17 11:55:51 -0400

    Create backup_workspace.sh

---
+++ backup_workspace.sh
@@ -0,0 +1,6 @@
+pushd /workspace
+find stable-diffusion-webui/embeddings/ stable-diffusion-webui/params.txt stable-diffusion-webui/ui-config.json stable-diffusion-webui/config.json stable-diffusion-webui/outputs stable-diffusion-webui/log stable-diffusion-webui/models -type f  -newer /workspace/workspace.7z > workspace_files.txt
+WORKSPACE_ARCHIVE=workspace-$(date +%s).7z
+7z a -p -mhe -ir@"workspace_files.txt" $WORKSPACE_ARCHIVE
+aws s3 cp $WORKSPACE_ARCHIVE s3://$S3_BUCKET/$WORKSPACE_ARCHIVE
+popd
