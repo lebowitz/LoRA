bce93d339e30e441455a55a18cc5bd41fc421e25
Craig Lebowitz <craiglebowitz@gmail.com>
2023-06-06 15:03:15 -0400

    Update train.sh

--- train.sh
+++ train.sh
@@ -5,11 +5,15 @@ cd ~/kohya_ss
 
 python -m venv venv
 source venv/bin/activate
+#pip3 install -r requirements.txt
+#pip3 install xformers
 
 export BASE_MODEL=v1-5-pruned.safetensors
+#export BASE_MODEL=realisticVisionV13_v13.safetensors
+
 export STABLE_DIFFUSION_WEBUI="/home/ubuntu/stable-diffusion-webui"
 export MODEL_NAME=$1
-export REPEATS=32
+export REPEATS=${2:-50}
 
 mkdir -v -p $STABLE_DIFFUSION_WEBUI/models/Lora/$MODEL_NAME
 T=$(date +%s)
@@ -19,18 +23,22 @@ mkdir -p $TMP
 mkdir -p $TMP/training
 
 cp -v ~/training/train.sh $TMP 
-cp -v -r /home/ubuntu/training/$MODEL_NAME/* "$TMP/training"
+cp -v -r ~/training/$MODEL_NAME/* "$TMP/training"
 
-# python ~/kohya_ss/finetune/make_captions.py $TMP/training
+pushd ~/kohya_ss
+python finetune/make_captions.py $TMP/training
+popd
 
 KK_DIR=`echo $REPEATS`_`echo $MODEL_NAME`
 mkdir -p $TMP/training_accept/$KK_DIR
-mkdir -p $TMP/training_reject
 
 cp -r -v $TMP/training/* $TMP/training_accept/$KK_DIR
 
+#python ~/training/extract_faces.py $TMP/training_accept/$KK_DIR
+
 find $TMP/training_accept -iname '*.jpg' -or -iname '*.png' > $TMP/training_filelist.txt
-montage @$TMP/training_filelist.txt -label %f -background '#336699' -geometry +4+4 $TMP/training_accept/training-summary.jpg
+
+montage @$TMP/training_filelist.txt -label %f -geometry +4+4 $TMP/training-summary.jpg
 
 python train_network.py \
      --network_module=networks.lora \
@@ -38,23 +46,30 @@ python train_network.py \
      --train_data_dir=$TMP/training_accept \
      --output_dir=$STABLE_DIFFUSION_WEBUI/models/Lora/$MODEL_NAME \
      --prior_loss_weight=0.9 \
+     --caption_extention=.txt \
      --resolution=512,512 \
      --network_dim=4 \
-     --learning_rate=1e-4\
-     --max_train_steps=5000 \
-     --cache_latents \
+     --learning_rate=7e-5 \
+     --lr_scheduler=constant \
      --mixed_precision="fp16" \
      --save_model_as="safetensors" \
+     --save_precision="fp16" \
      --output_name=$MODEL_NAME \
-     --save_every_n_epochs=1
+     --max_train_steps=2000 \
+     --cache_latents \
      --xformers \
-     --use_8bit_adam \
+     --train_batch_size=1 \
      --gradient_checkpointing \
+     --logging_dir=$TMP/logs \
+     --save_every_n_epochs=1
+     # --reg_data_dir=$STABLE_DIFFUSION_WEBUI/training/hope/reg \
+     #--use_8bit_adam \
+     #--gradient_checkpointing \
      #--mem_eff_attn
      #--save_state \
      #--xformers 
      #--debug_dataset
      #--mem_eff_attn \
-     # --reg_data_dir=$STABLE_DIFFUSION_WEBUI/training/hope/reg \
-
-/home/ubuntu/training/gen.sh realisticVisionV13_v13 $MODEL_NAME
+     #--save_every_n_epochs=1 \     
+#     --enable_bucket \
+#     --text_encoder_lr=1e-6 \
